c27e62ad5ef36371c1059ddd62db6b154088d856
CodeBaker <86.shashank@gmail.com>
2025-09-05 12:22:54 +0000

    commit

--- src/FunctionalInterface/PredicateConsumerEx.java
+++ src/FunctionalInterface/PredicateConsumerEx.java
@@ -2,19 +2,22 @@ package FunctionalInterface;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class PredicateConsumerEx {
     Predicate<Student> p1 = (s) -> s.getGradeLevel() >= 3;
     Predicate<Student> p2 = (s) -> s.getGpa() >= 3.9;
+
+    BiPredicate<Integer, Double> biPredicate = (grade, gpa) -> grade >= 3 && gpa >= 3.9;  
     
     BiConsumer<String,List<String>> studentBiConsumer = (name, activities) -> {
         System.out.println(name + " : " + activities);
 
     };
     Consumer<Student> StudentConsumer = (s) -> {
-        if(p1.and(p2).test(s)){
+        if(biPredicate.test(s.getGradeLevel(), s.getGpa())){
             studentBiConsumer.accept(s.getName(), s.getActivities());
 
         }
